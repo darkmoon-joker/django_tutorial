22d0e6db79a64ccb04d84b879a28f10c0d013e9e
Joker1995731 <149445778+Joker1995731@users.noreply.github.com>
2023-11-21 03:58:45 +0300

    Modified django admin page.

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rh*i0uu&hm5&9vq0s0c4ha_ojc(*uzz*@1)63eb3u-n0#e0qg9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
